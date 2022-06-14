00ca0b742d3492f92cb9f9d4e06f3447fe0229ec
Alexey Korkoza <alexeykorkoza@gmail.com>
2022-06-14 17:14:02 +0300

    feature: update remix config

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "netlify",
+  server: "./server.js",
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
